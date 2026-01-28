95379d5feeb8378b93812028a70935e41b1b18ab
Computhink-CS125 <cs125@computhink.com.sg>
2026-01-28 09:01:22 +0000

    lesson 4

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -28,22 +28,119 @@
 # print(2+4*5/6+3-6/2)
 
 # Task 3a
-a = 6
-b = 7
-ans = a + b
-print(ans)
+# a = 6
+# b = 7
+# ans = a + b
+# print(ans)
 
 # Task 3b
-a = 10
-b = 50
-c = b - a 
-ans = c * 3
-print(ans)
+# a = 10
+# b = 50
+# c = b - a 
+# ans = c * 3
+# print(ans)
 
 # Task 3c
-ans = 100 / 4
-print(ans)
+# ans = 100 / 4
+# print(ans)
       
 # Task 3d
-ans = (3+4)*5-(10/2)
-print(ans)
+# ans = (3+4)*5-(10/2)
+# print(ans)
+
+
+## Recap 1: Pseudo Code Practice
+# Write a pseudocode that calculates the average score of the 4
+# students:
+
+# Adam: 60
+# Brandon: 58
+# Charlie: 98
+# Daniel: 79
+
+# a = 60
+# b = 58
+# c = 98
+# d = 79
+# total = a + b + c + d
+# ave = total / 4
+# print ( ave )
+
+# put all the numbers into 4 variables (e.g. a = 60 b = 58...)
+#add all the variables together (a + b + c...)
+#divide the total by 4
+# print the answer
+
+
+# find out the remainder of two number in division
+# creating two variables num1 and num2 to find out the remainder of num1/num2
+# print out the remainder
+# num1 = 57
+# num2 = 2
+# print (num1%num2)
+
+
+## Task 3: Rounding practice
+
+# **Task 3a**:
+# Round 42.56789 to two decimal places.
+
+# **Task 3b**:
+# Round 1267.15 to the nearest integer.
+
+# **Task 3c**:
+# Round 1785.956 to the nearest hundreds.
+
+# print(round(42.56789,2))
+# print(round(1267.15,0))
+# print(round(1785.956,-2))
+
+## Task 4: Mathematics with variables
+# For each of the following questions, create variables to hold
+# the values given before finally using print(ans) to print the
+# answer:
+
+# **Task 4a**:
+# Create 2 variables 'a' and 'b', assign any number to
+# them and print their sum.
+
+# **Task 4b**:
+# Subtract 10 from 50, then multiply the result by 3.
+
+# **Task 4c**:
+# Divide 100 by 4 and print the result.
+
+# **Task 4d**:
+# Calculate and print the result of (3 + 4) * 5 - (10 / 2).
+
+## Task 5: Order of Operations practice
+# Code one Python expression to calculate and print the total cost for each task. Round the answer to 2 decimal places.
+
+# **Task 5a**:
+# - Two boxes of pineapple tarts - $15 each
+# - One packet of bak kwa for - $58
+
+# **Task 5b**:
+# A 10% service charge is applied, and a $25 off voucher is used, to the following order:
+# - One seafood platter - $96
+# - Three side dishes - $18 each
+# - Six bowls of rice - $2 each
+
+# **Task 5c**:
+# A 10% service charge is applied then, a 9% GST is applied, to the following order:
+# - Five seafood platters - $100 each
+# - Eight side dishes - $20 each
+# - Twenty bowls of rice - $2.50 each
+
+#Task 5a
+print(15*2+58)
+
+#Task 5b
+print(round((96+18*3+2*6)*1.1-25,2))
+
+#Task 5c
+print(round((500+160+20*2.5)*1.1*1.09,2))
+
+number = input("What is your favourite number?\n")
+print(number)
+
--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,68 @@
-print("Hello from lesson 4")
+# # Task 1a
+# name = input("What is your name?\n")
+# print(name)
+
+# # Task 1b
+# colour = input("What is your favourite colour?\n")
+# print(colour)
+
+# # Task 1c
+# age = input("What is your age?\n")
+# print(age)
+
+# name = input("What is your name?\n")
+# print("Hi, " + name + "! Have a good day")
+
+# #Task 2b
+# hobby = input("What is your favourite hobby?\n")
+# print("I enjoy " + hobby )
+
+#Task 2c
+# place = input("What is your dream vacation?\n")
+# print("I would love to go to " + place) 
+
+#Task 3a
+# age = int(input("What is your age?\n"))
+# age += 1
+# age = str(age)
+# print("Next year, you will be " + age + " years old")
+
+#Task 3b
+# number = int(input("What is your number\n"))
+# number *= 2
+# number = str(number)
+# print("Double the number is: " + number)
+
+#Task 3c
+# year_born = int(input("Which year were you born?\n"))
+# year_born = 2026-year_born
+# year_born = str(year_born)
+# print("You are " + year_born + " years old.")
+
+#Task 4
+# message = str(Hello)
+# message = int(10)
+
+#Task 5
+# firstName = ("Marco ")
+# lastName = ("Ong")
+# print(firstName + lastName)
+
+#Task 6
+# user_name = input("Name?\n")
+# user_age = int(input("Age?\n"))
+# user_age = str(user_age)
+# print(user_name + " is " + user_age + " years old.")
+
+#Task 7
+# length = int(input("What is the length of the rectsngle?\n"))
+# width = int("What is the width of the rectsngle?\n")
+# area = length * width
+# print(width)
+
+#Task 8
+min = int(input("How many minutes?\n)")
+sec = int(("How many seconds?\n"))
+min_to_sec = min*60
+ans = min + sec
+print(ans)
